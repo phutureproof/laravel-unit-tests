b0e73c6055fee489c7326921766cf61c1644c04d
PhutureProof <dale@phutureproof.com>
2017-10-24 14:04:21 +0100

    new service added new tests added for new service

---
+++ tests/Unit/NewExampleServiceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use Mockery\CountValidator\Exact;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\NewExampleService;
+
+class NewExampleServiceTest extends TestCase
+{
+    /** @var NewExampleService */
+    public $SUT;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->SUT = new NewExampleService();
+    }
+
+    public function test_service_has_properties()
+    {
+        $this->assertObjectHasAttribute('foo', $this->SUT);
+        $this->assertObjectHasAttribute('bar', $this->SUT);
+    }
+
+    public function test_service_sets_and_gets_data()
+    {
+        $this->SUT->setFoo('test');
+        $this->assertTrue($this->SUT->getFoo() === 'test');
+
+        $this->SUT->setBar('test');
+        $this->assertTrue($this->SUT->getBar() === 'test');
+
+    }
+}
